5be4a4030ad0c7997f7b2baa70a8671724c0a151
Jake-Zaruba <94652629+Jake-Zaruba@users.noreply.github.com>
2023-02-03 22:52:05 -0600

    Finished rough layout

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Attachment from "../img/attachment.svg";
+import Img from "../img/addImage.svg";
 
 const Input = () => {
-  return <div className="input">Input</div>;
+  return (
+    <div className="input">
+      <input type="text" placeholder="Type something..." />
+      <div className="send">
+        <img className="icon" src={Attachment} />
+        <input type="file" style={{ display: "none" }} id="file" />
+        <label htmlFor="file">
+          <img className="icon" src={Img} />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Person from "../img/person.jpeg";
+import ProfilePic from "../img/profile-pic.jpeg";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img className="profilePic" src={Person} />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img className="profilePic" src={ProfilePic} />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
