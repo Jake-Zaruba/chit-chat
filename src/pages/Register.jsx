3368fe8880a9fe0acc5ce0a0c9043ec6c10be0be
Jake-Zaruba <94652629+Jake-Zaruba@users.noreply.github.com>
2023-02-07 01:47:00 -0600

    Seting up useContext

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,9 +4,12 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, storage, db } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const displayName = e.target[0].value;
@@ -41,6 +44,7 @@ const Register = () => {
             });
 
             await setDoc(doc(db, "userChats", res.user.uid), {});
+            navigate("/");
           });
         }
       );
